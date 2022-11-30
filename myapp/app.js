e067b989b20386b4b10725a9475684d37756639e
JeniferAndrea TorresR <jenifer.torres@boatyardx.com>
2022-11-30 18:30:00 -0500

    :sparkles: Add node app

--- myapp/app.js
+++ myapp/app.js
@@ -3,12 +3,12 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Go to /user :)')
-  })  
+  res.send('Hello World!')
+})
 
 app.get('/user', (req, res) => {
-  res.send('Hello Users from Express')
-})
+    res.send('Hello Users from Express')
+  })
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
